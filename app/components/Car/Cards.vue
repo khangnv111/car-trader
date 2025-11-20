3c65ac2ba4a495f4def9a850b235a693e9e94355
Dominic Nguyenx <khangnv111@gmail.com>
2025-11-20 21:02:21 +0700

    dynamic rendering the car cards

--- app/components/Car/Cards.vue
+++ app/components/Car/Cards.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="w-full">
     <!-- CAR CARD -->
-    <Card/>
+    <Card v-for="car in cars" :key="car.id" :car="car"/>
     <!-- CAR CARD -->
   </div>
 </template>
 <script setup lang="ts">
 import Card from "~/components/Car/Card.vue";
+
+const {cars} = useCars();
+
 </script>
---
+++ app/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex h-screen justify-center items-center flex-col">
+    <h1 class="flex-9xl">{{ error.statusCode }}</h1>
+    <p class="mt-7 text-4xl">
+      {{ error.message }}
+    </p>
+    <button @click="handleError" class="rounded mt-7 text-2xl bg-blue-400 px-7 py-4 text-white">Go back</button>
+  </div>
+</template>
+
+<script setup>
+const error = useError();
+
+const handleError = () => {
+  navigateTo('/')
+}
+</script>
