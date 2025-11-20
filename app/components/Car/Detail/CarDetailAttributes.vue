03cda136cfa5dbc12108795b707374f26bf0ae3a
Dominic Nguyenx <khangnv111@gmail.com>
2025-11-20 21:09:55 +0700

    dynamic rendering detail

--- app/components/Car/Detail/CarDetailAttributes.vue
+++ app/components/Car/Detail/CarDetailAttributes.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="mr-10 mt-5 border-b pb-5">
-    <div class="flex text-lg mt-2">
+    <div class="flex text-lg mt-2" v-for="item in features" :key="item">
       <p class="rounded text-lime-800 mr-3">✔</p>
-      <p>Leather Interior</p>
+      <p>{{ item }}</p>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+const props = defineProps({
+  features: Array,
+})
 </script>
--- app/pages/car/[name]-[id].vue
+++ app/pages/car/[name]-[id].vue
@@ -1,12 +1,9 @@
 <template>
   <!-- CAR DETAIL PAGE -->
-  <div>
-    <CarDetailHero/>
-
-    <CarDetailAttributes/>
-
-    <CarDetailDescription/>
-
+  <div v-if="car">
+    <CarDetailHero :car="car" />
+    <CarDetailAttributes :features="car.features" />
+    <CarDetailDescription :description="car.description" />
     <CarDetailContact/>
   </div>
   <!-- CAR DETAIL PAGE   -->
@@ -17,10 +14,15 @@ import CarDetailDescription from "~/components/Car/Detail/CarDetailDescription.v
 import CarDetailContact from "~/components/Car/Detail/CarDetailContact.vue";
 
 const route = useRoute()
+const { cars } = useCars();
 const { toTitleCase } = useUtil()
 
 useHead({
   title: `${toTitleCase(route.params.name)}`,
+});
+
+const car = computed(() => {
+  return cars.find(car => car.id == parseInt(route.params.id));
 })
 
 definePageMeta({
